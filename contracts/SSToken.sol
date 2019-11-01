e01197aed131394e1f97454694eab1965626753c
cloudgray <bluejet1104@gmail.com>
2019-11-01 21:16:43 +0900

    ssToken basic setting completed

--- contracts/SSToken.sol
+++ contracts/SSToken.sol
@@ -1,10 +1,53 @@
 pragma solidity ^0.5.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+// import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract SSToken is ERC20, ERC20Detailed {
-	constructor(uint256 initialSupply) ERC20Detailed("ssuk-ssuk token", "SST", 18) public {
+contract SSToken is ERC20 {
+	string private _name;
+	string private _symbol;
+	uint8 private _decimals;
+
+	/**
+	* @dev Sets the values for `name`, `symbol`, and `decimals`. All three of
+	* these values are immutable: they can only be set once during
+	* construction.
+	*/
+	constructor(uint256 initialSupply, string name, ) public {
 		_mint(msg.sender, initialSupply);
+		_name = name;
+    _symbol = symbol;
+    _decimals = decimals;
+	}
+
+	/**
+		* @dev Returns the name of the token.
+		*/
+	function name() public view returns (string memory) {
+			return _name;
+	}
+
+	/**
+		* @dev Returns the symbol of the token, usually a shorter version of the
+		* name.
+		*/
+	function symbol() public view returns (string memory) {
+			return _symbol;
+	}
+
+	/**
+		* @dev Returns the number of decimals used to get its user representation.
+		* For example, if `decimals` equals `2`, a balance of `505` tokens should
+		* be displayed to a user as `5,05` (`505 / 10 ** 2`).
+		*
+		* Tokens usually opt for a value of 18, imitating the relationship between
+		* Ether and Wei.
+		*
+		* NOTE: This information is only used for _display_ purposes: it in
+		* no way affects any of the arithmetic of the contract, including
+		* {IERC20-balanceOf} and {IERC20-transfer}.
+		*/
+	function decimals() public view returns (uint8) {
+			return _decimals;
 	}
 }
